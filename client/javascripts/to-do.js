4f54ae3123df610679fdea8615a583d79fba7d84
lauren <lliebert@uoregon.edu>
2020-05-20 16:25:46 -0700

    Req 2

--- client/javascripts/to-do.js
+++ client/javascripts/to-do.js
@@ -1,6 +1,12 @@
 //jshint esversion: 6
 
 let controller = function() {
+  //console.log(localStorage.getItem("commentsList"))
+  //load comments from local storage when page loads
+  if (localStorage.getItem("commentsList")) {
+    $(".comments").html(localStorage.getItem("toDoList"));
+  }
+
   let addCommentFromInputBox = function() {
     //Semmy uses "$" to name variables that will contain jQuery objects
     let $new_comment;
@@ -11,6 +17,10 @@ let controller = function() {
       $(".comments").append($new_comment);
       //$new_comment.fadeIn();
       $(".comment-input input").val("");
+
+      //store the list of paragraph elements
+      localStorage.setItem("toDoList", $(".comments").html());
+      console.log(localStorage.getItem("commentsList"));
 
       //log the list of paragraph elements
       console.log($(".comments").html());
@@ -28,4 +38,16 @@ let controller = function() {
   });
 };
 
-$(document).ready(controller);
+let deleteHandler = () => {
+  console.log("dH");
+  localStorage.removeItem("commentsList");
+  window.location.reload();
+};
+
+$(document).ready(() => {
+  console.log("ready");
+  //selected the delete button
+  let buttonElem = document.querySelectorAll("button")[1];
+  buttonElem.addEventListener("click", deleteHandler);
+  controller();
+});
